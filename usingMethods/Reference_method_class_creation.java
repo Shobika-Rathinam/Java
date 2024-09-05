f812b95e0750853c490cdcb853631708d955c5e3
Shobika-Rathinam <shobikar2501@gmail.com>
2024-09-05 15:04:15 +0530

    Create Reference_method_class_creation.java

---
+++ usingMethods/Reference_method_class_creation.java
@@ -0,0 +1,14 @@
+class H{
+    int id;
+    String name;
+
+}
+class Hello{
+    public static void main(String[] args) {
+        H a=new H();
+        a.id=12;
+        a.name="siva";
+        System.out.println(a.id+" "+a.name);
+        
+    }
+}
