e13035773b379b64c998afd7f7909df07791ff47
Shobika-Rathinam <shobikar2501@gmail.com>
2024-09-06 14:49:43 +0530

    Create overloading by different arguments.java

---
+++ Polymorphism/overloading by different arguments.java
@@ -0,0 +1,13 @@
+
+public class loov {
+    public static void main(String[] args) {
+        System.out.println(add(5,5));
+        System.out.println(add(5,5,5));
+    }
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int add(int a, int b,int c) {
+        return a + b+c;
+    }
+}
