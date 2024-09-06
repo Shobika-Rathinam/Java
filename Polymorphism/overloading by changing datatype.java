2412037b412de7cfb0a4a4fdf0b2dddaf34dc578
Shobika-Rathinam <shobikar2501@gmail.com>
2024-09-06 15:03:56 +0530

    Create overloading by changing datatype.java

---
+++ Polymorphism/overloading by changing datatype.java
@@ -0,0 +1,12 @@
+public class loov {
+    public static void main(String[] args) {
+        System.out.println(add(5,5));
+        System.out.println(add(5,5,5));
+    }
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static float add(float a, float b,float c) {
+        return a + b+c;
+    }
+}
