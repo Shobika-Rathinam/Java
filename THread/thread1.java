1319bb78979ea01233d34a1ffd2616462869cb1e
Shobika-Rathinam <shobikar2501@gmail.com>
2024-09-12 17:07:17 +0530

    Create thread1.java

---
+++ THread/thread1.java
@@ -0,0 +1,16 @@
+
+public class thrd extends Thread {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		thrd m=new thrd();
+		Thread s2=new Thread("Siva");
+		System.out.println(s2.getName());
+	    Thread s3=new Thread("Krishna");
+	    System.out.println(s3.getName());
+	    
+		
+	}
+
+}
